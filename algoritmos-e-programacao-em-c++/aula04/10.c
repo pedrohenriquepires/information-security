1a1d63a0b483cf5d3e75d267ca63121aac7bf824
Pedro Henrique Pires <rikexd@gmail.com>
2017-03-14 14:52:26 -0300

    fix indentation

--- algoritmos-e-programacao-em-c++/aula04/10.c
+++ algoritmos-e-programacao-em-c++/aula04/10.c
@@ -4,20 +4,20 @@
 int main()
 {
 	int colunas;
-
+	
 	readInt("Informe a quantidade de colunas", &colunas);
-
+	
 	printf("\n------------------\n\n");
-
+	
 	for(int l = 0; l <= colunas; l++) {
-	    for(int c = colunas - l; c >= 0; c--) {
-            printf("* ");
-        }
-
-        printf("\n");
+		for(int c = colunas - l; c >= 0; c--) {
+			printf("* ");
+		}
+		
+		printf("\n");
 	}
-
+	
 	printf("\n------------------\n");
-
+	
 	return 0;
 }
